7ee2e95fdd018ac7790c0124c7eb8dfd26beccca
Yisus Web <connexion.jesus@gmail.com>
2025-11-20 19:57:37 -0500

    hjjjjj

--- resources/views/pages/product-description.blade.php
+++ resources/views/pages/product-description.blade.php
@@ -260,12 +260,12 @@
 							<div class="large-image  ">
 								<img itemprop="image" class="product-image-zoom" src="{{ asset($product->image) }}"  title="{{ $product->name }}" alt="{{ $product->name }}">
 							</div>
-							<a class="thumb-video pull-left" href="https://www.youtube.com/watch?v=HhabgvIIXik"><i class="fa fa-youtube-play"></i></a>
+							<a class="thumb-video pull-left" href="https://www.youtube.com/watch?v=lUWv2EnR74I"><i class="fa fa-youtube-play"></i></a>
 							
 							<div id="thumb-slider" class="yt-content-slider full_slider owl-drag" data-rtl="yes" data-autoplay="no" data-autoheight="no" data-delay="4" data-speed="0.6" data-margin="10" data-items_column00="4" data-items_column0="4" data-items_column1="3" data-items_column2="4"  data-items_column3="1" data-items_column4="1" data-arrows="yes" data-pagination="no" data-lazyload="yes" data-loop="no" data-hoverpause="yes">
 								
                                 @foreach ($product->images as $image)
-                                <a data-index="0" class="img thumbnail " data-image="{{ asset('storage/' . $image->image_path) }}" title="{{ $product->name }}">
+                                <a data-index="0" class="img thumbnail" data-image="{{ asset('storage/' . $image->image_path) }}" title="{{ $product->name }}">
 									<img src="{{ asset('storage/' . $image->image_path) }}" title="{{ $product->name }}" alt="{{ $product->name }}">
 								</a>
                                 @endforeach
