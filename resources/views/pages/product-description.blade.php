3001b4fae1e203d63305a62b24447d7c70d22e0c
Yisus Web <connexion.jesus@gmail.com>
2025-11-20 20:29:14 -0500

    ghh

--- resources/views/pages/product-description.blade.php
+++ resources/views/pages/product-description.blade.php
@@ -260,7 +260,7 @@
 							<div class="large-image  ">
 								<img itemprop="image" class="product-image-zoom" src="{{ asset($product->image) }}"  title="{{ $product->name }}" alt="{{ $product->name }}">
 							</div>
-							<a class="thumb-video pull-left" href="https://www.youtube.com/watch?v=lUWv2EnR74I"><i class="fa fa-youtube-play"></i></a>
+							<a class="thumb-video pull-left" href="{{ $product->additional3 }}"><i class="fa fa-youtube-play"></i></a>
 							
 							<div id="thumb-slider" class="yt-content-slider full_slider owl-drag" data-rtl="yes" data-autoplay="no" data-autoheight="no" data-delay="4" data-speed="0.6" data-margin="10" data-items_column00="4" data-items_column0="4" data-items_column1="3" data-items_column2="4"  data-items_column3="1" data-items_column4="1" data-arrows="yes" data-pagination="no" data-lazyload="yes" data-loop="no" data-hoverpause="yes">
 								
